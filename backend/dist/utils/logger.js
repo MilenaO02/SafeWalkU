4a21b37937c237eff44b3a8d3a966b8c00476fe0
MilenaO02 <miordonezle@uide.edu.ec>
2026-07-23 23:42:54 -0500

    Arreglos

--- backend/dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = logger;
-function logger(metodo, ruta) {
-    const fecha = new Date().toLocaleString();
-    console.log(`[${fecha}] ${metodo} ${ruta}`);
-}

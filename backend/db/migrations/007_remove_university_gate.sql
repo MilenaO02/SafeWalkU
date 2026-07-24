9bcd9fa86f1cb7382ba910b40b9774382d46548f
MilenaO02 <miordonezle@uide.edu.ec>
2026-07-24 00:47:28 -0500

    Arreglos

--- backend/db/migrations/007_remove_university_gate.sql
+++ backend/db/migrations/007_remove_university_gate.sql
@@ -1,17 +1,31 @@
 -- La garita universitaria no forma parte de la red de seguridad de SafeWalk U.
+START TRANSACTION;
+
 UPDATE reporte SET id_ubicacion = 1 WHERE id_ubicacion = 3;
 
+CREATE TEMPORARY TABLE rutas_garita (
+    id_ruta INT PRIMARY KEY
+);
+
+INSERT IGNORE INTO rutas_garita (id_ruta)
+SELECT id_ruta
+FROM ruta_ubicacion
+WHERE id_ubicacion = 3;
+
 DELETE rf FROM rutafavorita rf
-INNER JOIN ruta_ubicacion ru ON ru.id_ruta = rf.id_ruta
-WHERE ru.id_ubicacion = 3;
+INNER JOIN rutas_garita rg ON rg.id_ruta = rf.id_ruta;
 
 DELETE rp FROM ruta_punto rp
-INNER JOIN ruta_ubicacion ru ON ru.id_ruta = rp.id_ruta
-WHERE ru.id_ubicacion = 3;
+INNER JOIN rutas_garita rg ON rg.id_ruta = rp.id_ruta;
+
+DELETE ru FROM ruta_ubicacion ru
+INNER JOIN rutas_garita rg ON rg.id_ruta = ru.id_ruta;
 
 DELETE r FROM ruta r
-INNER JOIN ruta_ubicacion ru ON ru.id_ruta = r.id_ruta
-WHERE ru.id_ubicacion = 3;
+INNER JOIN rutas_garita rg ON rg.id_ruta = r.id_ruta;
 
 DELETE FROM lugarseguro WHERE LOWER(nombre) LIKE '%garita%';
 DELETE FROM ubicacion WHERE id_ubicacion = 3 OR LOWER(nombre) LIKE '%garita%';
+
+DROP TEMPORARY TABLE rutas_garita;
+COMMIT;
